2c3dfcf9e00ac065dac50f671581da98064cf49f
hsnavarro <ricksnavarro@gmail.com>
2021-09-23 18:29:54 -0400

    Update PBS

--- misc/pbs.cpp
+++ misc/pbs.cpp
@@ -1,10 +1,10 @@
-// Parallel Binary Search - O(nlog n * cost to update data structure + qlog n * cost for bs condition)
+// Parallel Binary Search - O(nlog n * cost to update data structure + qlog n * cost for binary search condition)
 
-struct query { int i, ans; /*+ query related info*/ };
-vector<query> req;
+struct Query { int i, ans; /*+ query related info*/ };
+vector<Query> req;
 int l_default, r_default, idx = l_default;
 
-void pbs(vector<query>& qs, int l_pbs = l_default, int r_pbs = r_default){
+void pbs(vector<Query>& qs, int l_pbs = l_default, int r_pbs = r_default){
     int mid = (l_pbs + r_pbs) / 2;
     // mid = (L+R+1)/2 if different from simple upper/lower bound
     if(qs.empty()) return;
@@ -19,14 +19,14 @@ void pbs(vector<query>& qs, int l_pbs = l_default, int r_pbs = r_default){
     }
 
     if(l_pbs == r_pbs) {
-        for(auto q : qs) req[q.i].ans = l_pbs;
+        for(auto& q : qs) req[q.i].ans = l_pbs;
         return;
     }
 
-    vector<query> vl, vr;
+    vector<Query> vl, vr;
     for (auto& q : qs) {
-        if ( /* cond */ ) vl.pb(q);
-        else vr.pb(q);
+        if ( /* cond */ ) vl.push_back(q);
+        else vr.push_back(q);
     }
 
     pbs(vl, l_pbs, mid);
