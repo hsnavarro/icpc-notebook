434d94a21912a1596e21a049e078b84333933731
Henrique Navarro <ricksnavarro@gmail.com>
2021-10-29 18:41:42 -0400

    update string hashing struct

--- strings/string-hashing-struct.cpp
+++ strings/string-hashing-struct.cpp
@@ -1,6 +1,6 @@
 // String Hashing
 // Rabin Karp - O(n + m)
-template <int N>
+template <int N = 3>
 struct Hash {
   int hs[N];
   static vector<int> mods;
@@ -11,40 +11,36 @@ struct Hash {
 
   Hash(int x = 0) { fill(hs, hs + N, x); }
 
-  bool operator==(const Hash& b) {
-    for (int i = 0; i < N; i++) if (hs[i] != b.hs[i]) return false;
-    return true;
+  bool operator<(const Hash& b) const {
+    for (int i = 0; i < N; i++) if (hs[i] < b.hs[i]) return true;
+    return false;
   }
 
-  bool operator!=(const Hash& b) {
-    return !(*this == b);
-  }
-
-  Hash operator+(const Hash& b) {
+  Hash operator+(const Hash& b) const {
     Hash ans;
     for (int i = 0; i < N; i++) ans.hs[i] = add(hs[i], b.hs[i], mods[i]);
     return ans;
   }
 
-  Hash operator-(const Hash& b) {
+  Hash operator-(const Hash& b) const {
     Hash ans;
     for (int i = 0; i < N; i++) ans.hs[i] = sub(hs[i], b.hs[i], mods[i]);
     return ans;
   }
 
-  Hash operator*(const Hash& b) {
+  Hash operator*(const Hash& b) const {
     Hash ans;
     for (int i = 0; i < N; i++) ans.hs[i] = mul(hs[i], b.hs[i], mods[i]);
     return ans;
   }
 
-  Hash operator+(int b) {
+  Hash operator+(int b) const {
     Hash ans;
     for (int i = 0; i < N; i++) ans.hs[i] = add(hs[i], b, mods[i]);
     return ans;
   }
 
-  Hash operator*(int b) {
+  Hash operator*(int b) const {
     Hash ans;
     for (int i = 0; i < N; i++) ans.hs[i] = mul(hs[i], b, mods[i]);
     return ans;
@@ -55,11 +51,16 @@ struct Hash {
     for (int i = 0; i < N; i++) ans.hs[i] = mul(b.hs[i], a, b.mods[i]);
     return ans;
   }
+
+  friend ostream& operator<<(ostream& os, const Hash& b) {
+    for (int i = 0; i < N; i++) os << b.hs[i] << " \n"[i == N - 1];
+    return os;
+  }
 };
 
 template <int N> vector<int> Hash<N>::mods = { (int) 1e9 + 9, (int) 1e9 + 33, (int) 1e9 + 87 }; 
 
-template <int N>
+template <int N = 3>
 struct PolyHash {
   vector<Hash<N>> h, p;
 
@@ -68,21 +69,21 @@ struct PolyHash {
     p.resize(sz + 1);
     h.resize(sz + 1);
 
-    p[0] = 1, p[1] = pr;
-    for (int i = 1; i <= sz; i++) {
-      h[i] = pr * h[i - 1] + s[i - 1];
+    p[0] = 1, h[0] = s[0];
+    for (int i = 1; i < sz; i++) {
+      h[i] = pr * h[i - 1] + s[i];
       p[i] = pr * p[i - 1];
     }
   }
 
-  Hash<N> fhash(int l, int r) {
-    l++, r++;
-    return h[r] - h[l - 1] * p[r - l + 1];
+  Hash<N> fhash(int l, int r) { 
+    if (!l) return h[r]; 
+    return h[r] - h[l - 1] * p[r - l + 1]; 
   }
 
-  Hash<N> shash(string& s) {
+  static Hash<N> shash(string& s, int pr = 313) {
     Hash<N> ans;
-    for (int i = 0; i < (int)s.size(); i++) ans = p[1] * ans + s[i];
+    for (int i = 0; i < (int)s.size(); i++) ans = pr * ans + s[i];
     return ans;
   }
 
@@ -96,7 +97,7 @@ struct PolyHash {
         cnt++;
       }
     }
-    
+
     return cnt;
   }
 };
