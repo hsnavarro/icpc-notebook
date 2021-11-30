b37be6dd84f07b17868e0ae34b29aefca5563a09
Henrique Soldi Navarro <42014408+hsnavarro@users.noreply.github.com>
2021-11-29 21:19:57 -0400

    Update manacher.cpp

--- strings/manacher.cpp
+++ strings/manacher.cpp
@@ -7,7 +7,7 @@
 vector<int> d1, d2;
 
 void manacher(string &s) {
-  int n = s.size();
+  int n = (int)s.size();
   d1.resize(n), d2.resize(n);
   for(int i = 0, l1 = 0, l2 = 0, r1 = -1, r2 = -1; i < n; i++) {
     if(i <= r1) d1[i] = min(d1[r1+l1-i], r1-i+1);
