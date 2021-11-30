ba00553cf6e07d07d893b623ab1adbd43b46c004
Henrique Soldi Navarro <42014408+hsnavarro@users.noreply.github.com>
2021-11-30 11:33:44 -0400

    Update z-function.cpp

--- strings/z-function.cpp
+++ strings/z-function.cpp
@@ -1,8 +1,8 @@
 // Z-Function - O(n)
 
-vector<int> z(string s){
+vector<int> zf(string s){
   vector<int> z(s.size());
-  for(int i = 1, l = 0, r = 0, n = s.size(); i < n; i++){
+  for(int i = 1, l = 0, r = 0, n = (int)s.size(); i < n; i++){
     if(i <= r) z[i] = min(z[i-l], r - i + 1);
     while(i + z[i] < n and s[z[i]] == s[z[i] + i]) z[i]++;
     if(i + z[i] - 1 > r) l = i, r = i + z[i] - 1;
