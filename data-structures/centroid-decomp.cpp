28d8e465ec23a35c62f03f5e6750d6ddce910688
Henrique Soldi Navarro <42014408+hsnavarro@users.noreply.github.com>
2021-12-14 12:20:51 -0400

    Update centroid-decomp.cpp

--- data-structures/centroid-decomp.cpp
+++ data-structures/centroid-decomp.cpp
@@ -1,33 +1,31 @@
 // Centroid Decomposition - O(nlog n)
 int n, m, sz[N], forb[N], par[N];
 
-void dfs(int u, int p) {
+int get_sz(int u, int p = -1) {
   sz[u] = 1;
-  for(auto v : adj[u]) {
-    if(v != p and !forb[v]) {
-      dfs(v, u);
-      sz[u] += sz[v];
-    }
-  }
+  for(int v : adj[u])
+    if(v != p and !forb[v]) sz[u] += get_sz(v, u);
+  
+  return sz[u];
 }
 
-int cent(int u, int p, int amt) {
-  for(auto v : adj[u]) {
+int cent(int amt, int u, int p = -1) {
+  for(int v : adj[u]) {
     if(v == p or forb[v]) continue;
-    if(sz[v] > amt/2) return cent(v, u, amt);
+    if(sz[v] > amt/2) return cent(amt, v, u);
   }
   return u;
 }
 
-void decomp(int u, int p) {
-  dfs(u, -1);
-  int cen = cent(u, -1, sz[u]);
+void decomp(int u = 1, int p = -1) {
+  dfs(u);
+  int cen = cent(sz[u], u);
   forb[cen] = 1;
   if(p != -1) par[cen] = p;
 
-  for(auto v : adj[u]) 
+  for(int v : adj[u]) 
     if(!forb[v]) decomp(v, cen);
 }
 
 // in main
-// decomp(1, -1);
+decomp();
