6f5354add58801bfec0847068286d91bd3d13961
Henrique Soldi Navarro <42014408+hsnavarro@users.noreply.github.com>
2021-12-14 12:51:06 -0400

    Update bit-struct.cpp

--- data-structures/bit-struct.cpp
+++ data-structures/bit-struct.cpp
@@ -5,10 +5,9 @@
 
 template <class T>
 struct Bit {
-  int n;
   vector<T> bit;
 
-  Bit(int _n) : n(_n), bit(n + 3) {}
+  Bit(int _n) : bit(_n + 3) {}
 
   void add(int p, T v) {
     for (p += 2; p < (int)bit.size(); p += p & -p) bit[p] += v;
