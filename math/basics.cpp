acfc3ae73e1e83b612e6ec579948836f3d1d77a5
Henrique Soldi Navarro <42014408+hsnavarro@users.noreply.github.com>
2022-04-19 14:06:58 -0300

    Create basics.cpp

---
+++ math/basics.cpp
@@ -0,0 +1,39 @@
+// Floor and Ceil
+int cceil(int a, int b) {
+  ll c = abs(a);
+  ll d = abs(b);
+  if (a * b > 0) return (c + d - 1)/d;
+  return -c/d;
+}
+ 
+int chao(int a, int b) {
+  ll c = abs(a);
+  ll d = abs(b);
+  if (a * b > 0) return c/d;
+  return -(c + d - 1)/d;
+}
+
+// Greatest Common Divisor & Lowest Common Multiple
+ll gcd(ll a, ll b) { return b ? gcd(b, a%b) : a; }
+ll lcm(ll a, ll b) { return a/gcd(a, b)*b; }
+
+// Multiply caring overflow
+ll mulmod(ll a, ll b, ll m = MOD) {
+  ll r=0;
+  for (a %= m; b; b>>=1, a=(a*2)%m) if (b&1) r=(r+a)%m;
+  return r;
+}
+
+// Another option for mulmod is using long double
+ull mulmod(ull a, ull b, ull m = MOD) {
+  ull q = (ld) a * (ld) b / (ld) m;
+  ull r = a * b - q * m;
+  return (r + m) % m;
+}
+
+// Fast exponential
+ll fexp(ll a, ll b, ll m = MOD) {
+  ll r=1;
+  for (a %= m; b; b>>=1, a=(a*a)%m) if (b&1) r=(r*a)%m;
+  return r;
+}
